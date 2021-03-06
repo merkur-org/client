8c85dd8047945f4f30942ad4c4604c97920017e2
Antonio Maccarini <antoniomaccarini@alunos.utfpr.edu.br>
2021-03-06 17:10:13 -0300

    feature: criar pagina de login com ajuste no tab

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,21 +2,48 @@ import {
   Container,
   BackgroundOrange,
   BackgroundWhiteRectangle,
-  OrangeLine,
-  SeparationLine,
-  InputContainer1,
-  InputContainer2
+  TabContainer,
+  FormContainer,
+  LinksContainer,
+  TabButton
 } from './styles'
 import { FaCheck } from 'react-icons/fa'
 import Button from '@/components/Button'
 import Input from '@/components/Input'
-import { useCallback, useRef } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 import getValidationErrors from '@/utils/getValidationErrors'
 function Login() {
+  const formTypes = [
+    {
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email'
+    },
+    {
+      name: 'senha',
+      type: 'password',
+      placeholder: 'Senha'
+    },
+    {
+      name: 'cpf',
+      type: 'number',
+      placeholder: 'CPF'
+    },
+    {
+      name: 'telefone',
+      type: 'number',
+      placeholder: 'Telefone'
+    }
+  ]
+
+  const [formInputs, setFormInputs] = useState([formTypes[0], formTypes[1]])
   const formRef = useRef<FormHandles>(null)
+
+  const [emailSelected, setEmailSelected] = useState(true)
+  const [cpfSelected, setCpfSelected] = useState(false)
 
   const handleSubmit = useCallback(async formData => {
     formRef.current?.setErrors({})
@@ -39,33 +66,58 @@ function Login() {
       }
     }
   }, [])
+
+  const handleChangeEmailTab = () => {
+    setFormInputs([formTypes[0], formTypes[1]])
+    setEmailSelected(true)
+    setCpfSelected(false)
+  }
+  const handleChangeCpfTab = () => {
+    setFormInputs([formTypes[2], formTypes[3]])
+    setEmailSelected(false)
+    setCpfSelected(true)
+  }
+
   return (
     <Container>
-      <BackgroundOrange />
       <BackgroundWhiteRectangle>
-        <OrangeLine />
-        <SeparationLine />
         <h1>BEM VINDO</h1>
         <h2>Informe seus dados para iniciar a sessão</h2>
-        <h3>email e senha</h3>
-        <h4>cpf e telefone</h4>
-        <h5>
-          <a href="forgotPassword">esqueceu sua senha?</a>
-        </h5>
-        <h6>
-          <a href="noRegister">não possui cadastro?</a>
-        </h6>
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <InputContainer1>
-            <Input name="Email" placeholder="Email" />
-          </InputContainer1>
-          <InputContainer2>
-            <Input name="Senha" type="password" placeholder="Senha" />
-          </InputContainer2>
+        <FormContainer>
+          <TabContainer>
+            <TabButton
+              isSelected={emailSelected}
+              onClick={handleChangeEmailTab}
+            >
+              email e senha
+            </TabButton>
+            <TabButton isSelected={cpfSelected} onClick={handleChangeCpfTab}>
+              cpf e telefone
+            </TabButton>
+          </TabContainer>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <Input
+              name={formInputs[0].name}
+              type={formInputs[0].type}
+              placeholder={formInputs[0].placeholder}
+            />
 
-          <Button text="Iniciar sessão" icon={FaCheck} type="submit" />
-        </Form>
+            <Input
+              name={formInputs[1].name}
+              type={formInputs[1].type}
+              placeholder={formInputs[1].placeholder}
+            />
+
+            <LinksContainer>
+              <a href="forgotPassword">esqueceu sua senha?</a>
+
+              <a href="noRegister">não possui cadastro?</a>
+            </LinksContainer>
+            <Button text="Iniciar sessão" icon={FaCheck} type="submit" />
+          </Form>
+        </FormContainer>
       </BackgroundWhiteRectangle>
+      <BackgroundOrange />
     </Container>
   )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Input from '@/components/Input'
 
 export const Container = styled.div`
@@ -7,124 +7,26 @@ export const Container = styled.div`
 
   display: flex;
   flex-direction: column;
-  place-content: center;
+  align-items: center;
+  justify-content: center;
 
   h1 {
     /* BEM VINDO */
-    width: 251px;
-    height: 47px;
-    margin-left: 32px;
-    margin-top: 32px;
 
-    font-family: Roboto Slab;
-    font-style: normal;
     font-weight: bold;
-    font-size: 36px;
-    line-height: 47px;
+    font-size: 3.6rem;
 
     /* Black Text */
-    color: #141211;
+    color: ${({ theme }) => theme.colors.black};
   }
   h2 {
     /* Informe seus dados para iniciar a sessão */
 
-    width: 366px;
-    height: 21px;
-    margin-left: 32px;
-    margin-top: 0px;
-
-    font-family: Roboto Slab;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 16px;
-    line-height: 21px;
+    font-weight: 400;
+    font-size: 1.6rem;
 
     /* Black Text */
-    color: #141211;
-  }
-  h3 {
-    /* email e senha */
-
-    width: 306px;
-    height: 21px;
-    margin-left: 32px;
-    margin-top: 48px;
-
-    font-family: Roboto Slab;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 16px;
-
-    /* identical to box height */
-
-    /* Black Text */
-    color: #141211;
-  }
-  h4 {
-    /* cpf e telefone */
-
-    width: 306px;
-    height: 21px;
-    margin-left: 135px;
-    margin-top: -20px;
-
-    font-family: Roboto Slab;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 16px;
-
-    /* identical to box height */
-
-    color: #888888;
-  }
-  h5 {
-    /* esqueceu sua senha? */
-
-    width: 306px;
-    height: 21px;
-    margin-left: 32px;
-    margin-top: 146px;
-
-    font-family: Roboto Slab;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 16px;
-
-    /* identical to box height */
-
-    color: #888888;
-  }
-  h6 {
-    /* não possui cadastro? */
-
-    width: 306px;
-    height: 21px;
-    margin-left: 313px;
-    margin-top: -21px;
-
-    font-family: Roboto Slab;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 12px;
-    line-height: 16px;
-
-    }
-
-    /* identical to box height */
-
-    color: #888888;
-  }
-
-  Button {
-    font-size: 12px;
-    width: 220px;
-    height: 48px;
-    margin-left: 30px;
-    margin-top: 0px;
-
+    color: ${({ theme }) => theme.colors.black};
   }
 `
 export const InputContainer1 = styled.div`
@@ -141,52 +43,70 @@ export const InputContainer2 = styled.div`
   height: 37px;
   }
 `
-export const OrangeLine = styled.div`
-  /* Line 11 */
 
-  position: absolute;
-  width: 96px;
-  height: 0px;
-  margin-left: 23px;
-  margin-top: 135px;
-
-  border: 4px solid #f7c80c;
-`
-export const SeparationLine = styled.div`
-  /* Line 10 */
-
-  position: absolute;
-  width: 464px;
-  height: 0px;
-  margin-left: 0px;
-  margin-top: 132px;
-
-  border: 1px solid #efeded;
-`
 export const BackgroundOrange = styled.div`
   /* Rectangle 25 */
-
   position: absolute;
+  top: 0;
+  height: 41.3%;
   width: 100%;
-  height: 278px;
-  left: 0px;
-  top: 0px;
 
-  background: #f59100;
+  z-index: -100;
+
+  background: ${({ theme }) => theme.colors.orangePrimary};
 `
 export const BackgroundWhiteRectangle = styled.div`
   /* Rectangle 24 */
 
-  position: absolute;
-
-  width: 464px;
-  height: 430px;
-  margin-left: 488px;
-  margin-top: -115px;
-
-  background: #ffffff;
+  background: ${({ theme }) => theme.colors.white};
+  padding: 3.2rem;
 
   /* Drop Shadow */
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
+`
+interface buttonProps {
+  isSelected: boolean
+}
+export const TabContainer = styled.div`
+  margin-bottom: 4.8rem;
+  display: flex;
+  justify-content: flex-start;
+`
+export const TabButton = styled.button<buttonProps>`
+  background: transparent;
+  border-top: 4px solid transparent;
+  &:active {
+    outline: none;
+    border: none;
+  }
+  &:focus {
+    border: none;
+    outline: 0 !important;
+  }
+
+  margin: 0;
+
+  padding: 0 1.2rem;
+  padding-top: 1.6rem;
+  ${props =>
+    props.isSelected
+      ? css`
+          border-top: 4px solid ${({ theme }) => theme.colors.yellowPrimary};
+        `
+      : css`
+          color: ${({ theme }) => theme.colors.gray};
+        `}
+`
+export const FormContainer = styled.div`
+  margin-top: 3.2rem;
+  border-top: 1px solid #efeded;
+
+  width: 100 %;
+`
+export const LinksContainer = styled.div`
+  margin-top: 0.8rem;
+  margin-bottom: 2.4rem;
+  display: flex;
+  justify-content: space - between;
 `
