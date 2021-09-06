96e68c65b2f0ffa986cf882e3eb7f9f249c1d5a7
Daniel Gustavo Favero <daniel.favero@contasimples.com>
2021-09-06 15:17:11 -0300

    fix: fixed list requests :hammer:

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 console.log({ apiURL: process.env.NEXT_PUBLIC_API_URL })
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://165.227.214.134/api'
+  baseURL: 'http://localhost:3333/api'
 })
 
 export default api
